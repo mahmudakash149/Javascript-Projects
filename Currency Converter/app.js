5609af1ef9853d0ca99ec1580d4d05322428b355
Nur Mohammod <nurmohammod993@gmail.com>
2024-11-16 12:00:52 +0600

    problem fixed

--- Currency Converter/app.js
+++ Currency Converter/app.js
@@ -1,6 +1,3 @@
-const BASE_URL =
-  "https://api.exchangerate-api.com/v4/latest";
-
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
 const fromCurr = document.querySelector(".from select");
@@ -26,28 +23,25 @@ for (let select of dropdowns) {
 }
 
 const updateExchangeRate = async () => {
-    let amount = document.querySelector(".amount input");
-    let amtVal = amount.value;
-    if (amtVal === "" || amtVal < 1) {
-      amtVal = 1;
-      amount.value = "1";
-    }
-  
-    const apiKey = "ebd82f36e6d3ae41aabf81b3";
-    const url = `${BASE_URL}?access_key=${apiKey}&symbols=${toCurr.value.toLowerCase()}`;
-  
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const rate = data.rates[toCurr.value.toLowerCase()];
-  
-      let finalAmount = amtVal * rate;
-      msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
-    } catch (error) {
-      console.error("Error fetching exchange rate:", error);
-      msg.innerText = "Error fetching exchange rate. Please try again.";
-    }
-  };
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+  const url = `https://api.exchangerate-api.com/v4/latest/${fromCurr.value}`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const rate = data.rates[toCurr.value];
+
+    let finalAmount = amtVal * rate;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+  } catch (error) {
+    console.error("Error fetching exchange rate:", error);
+    msg.innerText = "Error fetching exchange rate. Please try again.";
+  }
+};
 
 const updateFlag = (element) => {
   let currCode = element.value;
